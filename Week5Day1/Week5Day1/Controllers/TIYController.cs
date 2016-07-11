6787c043d496cfd8c4d920851bb39e9a2da223cb
Ronda Bergman <rondabergman@hotmail.com>
2016-07-11 13:59:02 -0400

    In class example

---
+++ Week5Day1/Week5Day1/Controllers/TIYController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week5Day1.Controllers
+{
+    public class TIYController : Controller
+    {
+        // GET: TIY
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Welcome(string name, int id = 1)
+        {
+            //return HttpUtility.HtmlEncode("Hello my name is " + name + "; ID = " + id);
+
+            ViewBag.Name = name;
+            ViewBag.ID = id;
+
+            return View();
+        }
+    }
+}
---
+++ Week5Day1/Week5Day1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week5Day1.MvcApplication" Language="C#" %>
